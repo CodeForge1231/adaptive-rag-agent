670f7721e7ee794216956d3a4c9986b0645e55ae
CodeForge1231 <sozanskiyandriyj@gmail.com>
2026-05-21 16:23:00 +0300

    chore: add Streamlit config and app metadata constants

---
+++ ui-streamlit/constants/metadata.py
@@ -0,0 +1,21 @@
+# Categorization of users by age group
+AGE_GROUPS = [
+    "teen",
+    "adult",
+    "senior",
+]
+
+# Definition of technical proficiency scales
+LEVELS = [
+    "beginner",
+    "intermediate",
+    "advanced",
+]
+
+# Supported technical domains for content filtering
+TOPICS = [
+    "python",
+    "ml",
+    "dl",
+    "nlp",
+]
